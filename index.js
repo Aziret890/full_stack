54b0aa57afba763e2ab7b039e7ab0cbbe4b18dd4
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-08 11:38:05 +0600

    update_mongodb_connect

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ app.get('/hi', (req, res) => {
 
 const start = async () => {
 	try {
-		
+		await mongoose.connect(process.env.DB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
 		app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
 	} catch (e) {
 		console.log(e)
