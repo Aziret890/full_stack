c9a5c2bd424b70a4c7f368d5fc0f43f05b766ec2
Janybek2007 <lr.arek.2007@gmail.com>
2024-05-08 11:33:25 +0600

    first_commit

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+
+const errorMiddleware = require('./middlewares/err-middleware')
+
+const PORT = process.env.PORT || 6500
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+	cors({
+		origin: '*',
+		credentials: true
+	})
+)
+app.use(errorMiddleware)
+
+app.get('/', (req, res) => {
+	const { name = 'world' } = req.query
+	res.json({
+		msg: `hello ${name}`
+	})
+})
+
+app.get('/hi', (req, res) => {
+	res.sendFile(__dirname + '/welcome/welcome.html')
+})
+
+const start = async () => {
+	try {
+		await mongoose.connect(process.env.DB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start().then(r => r)
+
+// module.exports = app
