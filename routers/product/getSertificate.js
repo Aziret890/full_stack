aa0560db906f9c34acf665f030015c2c05c4d80e
aziret <kubaaziret68@gmail.com>
2024-05-28 05:59:11 +0600

    update getAllProduct

--- routers/product/getSertificate.js
+++ routers/product/getSertificate.js
@@ -1,10 +1,11 @@
 import productModel from "../../models/product.js";
-async function getAllProduct(res) {
+
+async function getAllProduct(req,res) {
   try {
     const getAllPosts = await productModel.find().exec();
     res.json({ data: getAllPosts });
   } catch (error) {
-    console.log(error, "error ");
+    console.log(error, "error");
   }
 }
 
